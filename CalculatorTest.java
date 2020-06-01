766a16e7bbe8795a75e2a3b339bf9b76670fdd0d
a-heard <62103675+a-heard@users.noreply.github.com>
2020-06-01 11:38:31 -0400

    Throw exception if negative number is used

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -7,9 +7,14 @@ public class CalculatorTest {
 
     // Use this annotation( ) when you're expecting a RuntimeException, not any kind
     // of returned value
-    @Test(expected = RuntimeException.class)
-    public final void whenMoreThan2NumbersAreUsedThenAnExceptionIsThrown() {
-        Calculator.add("1,2,3");
+    // @Test(expected = RuntimeException.class)
+    // public final void whenMoreThan2NumbersAreUsedThenAnExceptionIsThrown() {
+    // Calculator.add("1,2,3");
+    // }
+
+    @Test
+    public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
+        Assert.assertEquals(1 + 2 + 3 + 4 + 5 + 6 + 7, Calculator.add("1,2,3,4,5,6,7"));
     }
 
     @Test
@@ -26,6 +31,21 @@ public class CalculatorTest {
     @Test
     public final void whenEmptyStringIsUsedThenReturnValue0() {
         Assert.assertEquals(0, Calculator.add(""));
+    }
+
+    @Test
+    public final void whenOneNumberIsUsedThenReturnValueIsSameNumber() {
+        Assert.assertEquals(10, Calculator.add("10"));
+    }
+
+    @Test
+    public final void whenTwoNumbersAreUsedReturnValueIsTheirSum() {
+        Assert.assertEquals(3 + 6, Calculator.add("3,6"));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public final void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown() {
+        Calculator.add("3,-6,9");
     }
 
 }
